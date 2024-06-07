09e318d192ed205f6fcadc950da502e5e0994305
Jianwen <jwu287@ucsc.edu>
2024-06-06 18:45:10 -0700

    Got skeleton done of the infinite runner where the player runs on a infinite surface

--- src/Scenes/Load.js
+++ src/Scenes/Load.js
@@ -11,16 +11,17 @@ class Load extends Phaser.Scene {
 
         // Load tilemap information
         this.load.image("tilemap_tiles", "tilemap_packed.png");                         // Packed tilemap
+        this.load.image("platform", "platform.png");                         
         //this.load.image("tilemap_tiles", "platformPack_tilesheet.png");                         // Packed tilemap
         //this.load.tilemapTiledJSON("platformer-level-1", "platformer-level-1.tmj");   // Tilemap in JSON
 
 
         // Load the tilemap as a spritesheet
         //this.load.spritesheet("tilemap_sheet", "platformPack_tilesheet.png", {
-        // this.load.spritesheet("tilemap_sheet", "tilemap_packed.png", {
-        //     frameWidth: 18,
-        //     frameHeight: 18,
-        // });
+        this.load.spritesheet("tilemap_sheet", "tilemap_packed.png", {
+             frameWidth: 18,
+             frameHeight: 18,
+        });
 
         // Load Sound
         this.load.audio("jump", "cartoon-jump.mp3");
