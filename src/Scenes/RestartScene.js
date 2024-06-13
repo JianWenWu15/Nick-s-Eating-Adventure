b84f625a742ccffea579b9af439cbf7735beff92
trinwu <trini.me28@gmail.com>
2024-06-12 22:29:53 -0700

    modified restart screen

--- src/Scenes/RestartScene.js
+++ src/Scenes/RestartScene.js
@@ -5,7 +5,14 @@ class RestartScene extends Phaser.Scene {
 
     create() {
         // Display a restart message
-        this.add.text(100, 100, 'Game Over. Press Space to Restart', { fill: '#ffffff' });
+        this.add.text(100, 100, 'Game Over. Press Space to Restart', { fill: '#ffffff' })
+            .setFontSize(35);
+        this.add.text(100, 600, 'Credits: Ian Liu, Jianwen Wu, Trinity Wu', { fill: '#ffffff' })
+            .setFontSize(25);
+        this.add.text(550, 200, "Score")
+            .setFontSize(50);
+
+        this.cameras.main.setBackgroundColor("#bae9ff");
 
         // Wait for the player to press Space
         this.input.keyboard.on('keydown-SPACE', () => {
